4fe8677156b20d3f6a205e84f05f06c85078d138
pedrosanbit <61233911+pedrosanbit@users.noreply.github.com>
2021-05-25 15:07:45 -0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -207,7 +207,7 @@
           
   			</div>
 		</div>
-		<script src="admin.js"></script>
+		<script src="js/admin.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
 	</body>
 </html>
